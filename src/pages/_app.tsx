007d906bbfa77ffe2a42f7b35494b8f3cce147ab
Sunny <sunnysinghrajavatssr@gmail.com>
2023-03-21 03:04:12 +0530

    disable animation on server-side

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { interFont } from "@/lib/fonts";
 import "@/styles/globals.css";
+import { MotionConfig } from "framer-motion";
 import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [reducedMotion, setReducedMotion] = useState(true);
+
+  useEffect(() => {
+    setReducedMotion(false);
+  }, []);
+
   return (
-    <main className={interFont.className}>
-      <Component {...pageProps} />
-    </main>
+    <MotionConfig reducedMotion={reducedMotion ? "never" : "always"}>
+      <main className={interFont.className}>
+        <Component {...pageProps} />
+      </main>
+    </MotionConfig>
   );
 }

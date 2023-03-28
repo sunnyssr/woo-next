35418ed7d280d11c0bbd6feda8c9f4c361ff34a8
Sunny <sunnysinghrajavatssr@gmail.com>
2023-03-28 08:37:49 +0530

    update vercel config for gravatar images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [process.env["WORDPRESS_ASSETS_DOMAIN"], "2.gravatar.com"],
+    domains: [process.env["WORDPRESS_ASSETS_DOMAIN"], "gravatar.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.gravatar.com",
+      },
+    ],
   },
   reactStrictMode: true,
 };

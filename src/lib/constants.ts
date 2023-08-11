5557b65a7fa77484f56a2e39c65a74c53a516a70
Sunny <sunnysinghrajavatssr@gmail.com>
2023-08-11 06:20:41 +0530

    Add WP credentials for getting post in edit context

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,7 @@
-export const WPBaseUrl = process.env.WORDPRESS_SITE_URL
+export const WPBaseUrl = process.env.WORDPRESS_SITE_URL;
 
-export const WooConsumerKey = process.env.WOO_CONSUMER_KEY
-export const WooConsumerSecret = process.env.WOO_CONSUMER_SECRET
+export const WooConsumerKey = process.env.WOO_CONSUMER_KEY;
+export const WooConsumerSecret = process.env.WOO_CONSUMER_SECRET;
+
+export const WPApplicationUsername = process.env.WP_APPLICATION_USERNAME;
+export const WPApplicationPassword = process.env.WP_APPLICATION_PASSWORD;
